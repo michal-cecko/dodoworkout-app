9fe7c6371aaefb7afca2e4f2ed47087764a9ee34
michalcecko <ceckomichal@gmail.com>
2025-01-19 18:59:37 +0100

    Header start + icons

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,3 +1,17 @@
 <header>
-    Header
+    <div class="container">
+        <div id="hamburger">
+            <span></span>
+        </div>
+        {!! svgIcon(public_path("logo/logo-black-red.svg"), ['class' => ['logo']]) !!}
+        <ul class="nav">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Trainings</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+    </div>
 </header>
