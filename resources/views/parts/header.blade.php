8fc26f2cc87a8b75256f5a4c9e2122db0de594d4
michalcecko <ceckomichal@gmail.com>
2025-01-19 19:14:27 +0100

    Header html

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -3,15 +3,44 @@
         <div id="hamburger">
             <span></span>
         </div>
-        {!! svgIcon(public_path("logo/logo-black-red.svg"), ['class' => ['logo']]) !!}
-        <ul class="nav">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Trainings</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
 
+        {!! svgIcon(public_path("logo/logo-black-red.svg"), ['class' => ['logo']]) !!}
+
+        <div class="nav-container">
+
+            <div class="searchbar">
+                {!! svgIcon(public_path("icon/icon-search.svg"), ['class' => ['search-icon']]) !!}
+                <input type="text" placeholder="{{__("searchbar.placeholder")}}">
+            </div>
+
+            <ul class="nav">
+                <li><a href="#">{{__("nav.about")}}</a></li>
+                <li><a href="#">{{__("nav.blog")}}</a></li>
+                <li><a href="#">{{__("nav.trainings")}}</a></li>
+                <li><a href="#">{{__("nav.events")}}</a></li>
+                <li><a href="#">{{__("nav.shop")}}</a></li>
+                <li><a href="#">{{__("nav.contact")}}</a></li>
+                <li><a href="#">{{app()->currentLocale() === "sk" ? "EN" : "SK"}}</a></li>
+            </ul>
+
+            <div class="socials-container">
+                @include("parts.socials")
+            </div>
+        </div>
+
+        <div class="icons-container">
+            <div class="icon-container">
+                <a href="#">{!! svgIcon(public_path("icon/icon-search.svg"), ['class' => ['search-icon']]) !!}</a>
+            </div>
+            <div class="icon-container">
+                <a href="#">{!! svgIcon(public_path("icon/icon-cart.svg"), ['class' => ['cart-icon']]) !!}</a>
+            </div>
+            <div class="icon-container">
+                <a href="#">{!! svgIcon(public_path("icon/icon-profile.svg"), ['class' => ['profile-icon']]) !!}</a>
+            </div>
+            <div class="icon-container">
+                <a href="#">{{app()->currentLocale() === "sk" ? "EN" : "SK"}}</a>
+            </div>
+        </div>
     </div>
 </header>
---
+++ resources/views/parts/socials.blade.php
@@ -0,0 +1,4 @@
+{!! svgIcon(public_path("icon/icon-instagram.svg"), ['class' => ['instagram-icon']]) !!}
+{!! svgIcon(public_path("icon/icon-facebook.svg"), ['class' => ['facebook-icon']]) !!}
+{!! svgIcon(public_path("icon/icon-youtube.svg"), ['class' => ['youtube-icon']]) !!}
+{!! svgIcon(public_path("icon/icon-tiktok.svg"), ['class' => ['tiktok-icon']]) !!}
